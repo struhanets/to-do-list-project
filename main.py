cba73e98e62dd11070b7240cb9597120cfd57191
struhanets <posseydon87@gmail.com>
2025-09-06 16:56:55 +0200

    Added endpoints

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from typing import List
+
 from fastapi import FastAPI
+
+from schemas import TaskCreate, TaskResponseData
+
+import crud
 
 app = FastAPI()
 
@@ -8,6 +14,26 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/tasks", response_model=TaskCreate)
+async def create_task(new_task: TaskCreate):
+    new_task_created = await crud.create_task(new_task)
+    return new_task_created
+
+
+@app.get("/tasks", response_model=List[TaskResponseData])
+async def get_tasks():
+    tasks = await crud.get_tasks()
+    return tasks
+
+
+@app.put("/tasks/{task_id}", response_model=TaskCreate)
+async def update_task(task_id: int, new_task_data: TaskCreate):
+    new_task = await crud.update_task(task_id, new_task_data)
+    return new_task
+
+
+@app.delete("/tasks/{task_id}", response_model=TaskResponseData)
+async def delete_task(task_id: int):
+    deleted_task = await crud.delete_task(task_id)
+
+    return deleted_task
