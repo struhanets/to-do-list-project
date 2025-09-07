273384e21b0a6de1b66b96668dc43ad9b4a9eac7
struhanets <posseydon87@gmail.com>
2025-09-07 11:24:45 +0200

    Tests was fixed

--- test_main.py
+++ test_main.py
@@ -1,8 +1,6 @@
 import pytest
-from fastapi import FastAPI
 
 from fastapi.testclient import TestClient
-from httpx import AsyncClient
 
 from datetime import datetime
 from main import app
@@ -11,19 +9,22 @@ from crud import tasks_storage
 client = TestClient(app)
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def clear_storage():
     tasks_storage.clear()
-    yield
+    with TestClient(app) as c:
+        yield c
     tasks_storage.clear()
 
 
-def test_hello_world():
+def test_main_page():
     response = client.get("/")
+    message = response.json().get("message")
     assert response.status_code == 200
+    assert message == "Welcome to New Page"
 
 
-def test_add_task():
+def test_add_task(clear_storage):
     mock_data = {
         "name": "Test Task",
         "description": "Some description",
@@ -40,9 +41,10 @@ def test_add_task():
     assert "id" in json_data
     assert json_data["name"] == mock_data["name"]
     assert json_data["status"] == mock_data["status"]
+    assert len(tasks_storage) == 1
 
 
-def test_update_task():
+def test_update_task(clear_storage):
     mock_data = {
         "name": "Test Task",
         "description": "Some description",
@@ -51,9 +53,10 @@ def test_update_task():
         "priority": "low"
     }
 
-    client.post("/tasks/", json=mock_data)
+    response = client.post("/tasks/", json=mock_data)
+    task_id = response.json().get("id")
     response = client.put(
-        "/tasks/1",
+        f"/tasks/{task_id}",
         json={
             "name": "Task",
             "description": "Some description",
@@ -65,13 +68,13 @@ def test_update_task():
 
     assert response.status_code == 200
     json_data = response.json()
-    assert json_data["id"] == 1
+    assert json_data["id"] == task_id
     assert json_data["name"] == "Task"
     assert json_data["status"] == "completed"
     assert json_data["priority"] == "high"
 
 
-def test_delete_task():
+def test_delete_task(clear_storage):
     mock_data = {
         "name": "Test Task",
         "description": "Some description",
@@ -80,10 +83,14 @@ def test_delete_task():
         "priority": "low"
     }
 
-    client.post("/tasks/", json=mock_data)
-    response = client.delete("/tasks/1")
-    assert response.status_code == 200
-    json_data = response.json()
-    assert json_data["id"] == 1
-    assert json_data["name"] == "Test Task"
+    response = client.post("/tasks/", json=mock_data)
+    task_id = response.json()["id"]
 
+    delete_resp = client.delete(f"/tasks/{task_id}")
+    assert delete_resp.status_code == 200
+
+    deleted_data = response.json()
+    print(deleted_data)
+    assert deleted_data["id"] == task_id
+    assert deleted_data["name"] == "Test Task"
+    assert len(tasks_storage) == 0
