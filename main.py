6ec162013e8c54fea79458deb5dd40cdca811293
struhanets <posseydon87@gmail.com>
2025-09-07 11:21:25 +0200

    Changed welcome message

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to New Page"}
 
 
 @app.post("/tasks", response_model=TaskResponseData)
