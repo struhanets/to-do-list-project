9c517998194ea57809ec7dac36e6fa64ec6c9e35
struhanets <posseydon87@gmail.com>
2025-09-08 09:26:46 +0200

    Fixed broker & backend servers in worker

--- celery_worker.py
+++ celery_worker.py
@@ -2,7 +2,7 @@ from celery import Celery
 
 celery_app = Celery(
     "worker",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0",
+    broker="redis://redis:6379/0",
+    backend="redis://redis:6379/0",
     include=["task_parser"],
 )
